24ece5b2c5aa79784547a6d4b7fdafc9edb81c20
RamyaJupaka <56325048+RamyaJupaka@users.noreply.github.com>
2020-02-20 23:13:49 -0500

    first

---
+++ sem2_project1/sem2_project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem2_project1
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine(Number(3, 9));
+            //    Console.WriteLine(Number(3, 3));
+            //    Console.ReadLine();
+
+            int num1 = 0, num2 = 0, res = 0;
+            Console.Write("Enter the first number :");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            res = num1 + num2;
+            Console.WriteLine("The result is : " + res);
+            Console.Read();
+            Console.ReadKey();
+            Console.ReadLine();
+
+        }
+
+    }
+    }
